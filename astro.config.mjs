b579d7dd7db910f438e005ecfb792ebeacddcf26
Nathan Parker <natecoleparker@gmail.com>
2023-12-20 21:31:33 -0500

    Adding Web Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+import vercelStatic from '@astrojs/vercel/static';
+ 
+export default defineConfig({
+	output: 'static',
+	adapter: vercelStatic({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+});
