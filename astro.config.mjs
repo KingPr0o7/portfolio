c7b33a615bf6b2f5e014aff3df693852861e09ec
Nathan Parker <natecoleparker@gmail.com>
2024-01-05 21:52:53 -0500

    🔗 URL Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 import { defineConfig } from 'astro/config';
 import vercelStatic from '@astrojs/vercel/static';
- 
+import metaTags from "astro-meta-tags";
+
+// https://astro.build/config
 export default defineConfig({
 	output: 'static',
 	site: 'https://portfolio-ncp.vercel.app/',
 	adapter: vercelStatic({
 		webAnalytics: {
-			enabled: true,
+			enabled: true
 		},
 		speedInsights: {
-			enabled: false,
-		},
+			enabled: false
+		}
 	}),
+	integrations: [metaTags()],
+	build: {
+		trailingSlash: 'never',
+	}
 });
