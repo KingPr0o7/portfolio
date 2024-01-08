10182c37ed63ad1407f27cad08bf4a5f52cd65a6
Nathan Parker <natecoleparker@gmail.com>
2024-01-08 00:07:42 -0500

    🖼️ Image Reductions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from 'astro/config';
-import vercelStatic from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/static';
 import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
 	site: 'https://portfolio-ncp.vercel.app/',
-	adapter: vercelStatic({
+	adapter: vercel({
 		webAnalytics: {
 			enabled: true
 		},
 		speedInsights: {
 			enabled: false
-		}
+		},
+		imageService: true,
 	}),
 	integrations: [metaTags()],
 	build: {
