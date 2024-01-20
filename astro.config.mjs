5bedc41f8b484615028b60d5736c73c71f766c8b
Nathan Parker <natecoleparker@gmail.com>
2024-01-20 13:37:52 -0500

    ⚙️ Animation Improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,22 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
 import metaTags from "astro-meta-tags";
 import mdx from "@astrojs/mdx";
+import vtbot from "astro-vtbot";
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
 	site: 'https://portfolio-ncp.vercel.app/',
 	adapter: vercel({
-		webAnalytics: {
-			enabled: true
-		},
-		speedInsights: {
-			enabled: false
-		},
-		imageService: true
+	webAnalytics: {
+		enabled: true
+	},
+	speedInsights: {
+		enabled: false
+	},
+	imageService: true
 	}),
-	integrations: [metaTags(), mdx()],
+	integrations: [metaTags(), mdx(), vtbot()],
 	build: {
 		trailingSlash: 'never'
 	}
